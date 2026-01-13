1bcc10944974ba4009cf7f37be8d9bec71908213
JudeSafo <jude.safo@gmail.com>
2026-01-13 13:00:24 -0600

    SSDBaseline

--- docs/components/hero-device-launcher/hero-device-launcher.js
+++ docs/components/hero-device-launcher/hero-device-launcher.js
@@ -12,9 +12,9 @@
    */
   const state = {
     handlers: {
-      android: () => routeToDocsViaAccessGate('hero_device_android'),
-      mac: () => routeToDocsViaAccessGate('hero_device_mac'),
-      iphone: () => routeToDocsViaAccessGate('hero_device_iphone'),
+      android: () => console.log('[hero-device] android clicked'),
+      mac: () => console.log('[hero-device] mac clicked'),
+      iphone: () => console.log('[hero-device] iphone clicked'),
     },
   };
 
